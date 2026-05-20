941705441a4bd842e0f934e359462d84505fd760
lana shotashvili <shotashvililana@gmail.com>
2026-05-20 20:02:46 +0400

    initialize project with Vite, React, and ESLint setup, create Header Component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./Components/Header/Header.jsx";
+import logo from "./assets/logo.png";
+
+function App() {
+  return (
+    <>
+      <Header imgLogo={logo} title="MovieLab" />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,27 @@
+import "./Header.css";
+
+function Header(props) {
+  return (
+    <header>
+      <div className="header-container container">
+        <div className="header-logo">
+          <img src={props.imgLogo} alt="header-logo" className="logo-img" />
+          <h2 className="logo-title">{props.title}</h2>
+        </div>
+
+        <div className="search-container">
+          <input
+            type="search"
+            id="search-bar"
+            placeholder="Search movies..."
+            aria-label="Search movies"
+          />
+        </div>
+
+        <button className="theme-btn">Dark Mode</button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
