a1c21cc57f5f6dd32f690ffaffa2c2a004f3f7af
lana shotashvili <shotashvililana@gmail.com>
2026-05-21 15:15:14 +0400

    feat: add MovieSection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./Components/Header/Header.jsx";
+import MovieSection from "./Components/MovieSection/MovieSection.jsx";
 import logo from "./assets/logo.png";
 
 function App() {
   return (
     <>
       <Header imgLogo={logo} title="MovieLab" />
+      <MovieSection />
     </>
   );
 }
---
+++ src/Components/MovieSection/MovieSection.jsx
@@ -0,0 +1,11 @@
+import "./MovieSection.css";
+
+function MovieSection() {
+  return (
+    <>
+      <h3>Movies</h3>
+    </>
+  );
+}
+
+export default MovieSection;
