3115142d80875be9c76eb0990d808191c6275429
lana shotashvili <shotashvililana@gmail.com>
2026-05-21 16:51:15 +0400

    feat: fetch movies from API and render movie cards

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         searchValue={searchValue}
         setSearchValue={setSearchValue}
       />
-      <MovieSection />
+      <MovieSection searchValue={searchValue} />
     </>
   );
 }
--- src/Components/MovieSection/MovieSection.jsx
+++ src/Components/MovieSection/MovieSection.jsx
@@ -1,12 +1,49 @@
+import { useEffect, useState } from "react";
 import "./MovieSection.css";
-// import MovieCard from "../MovieCard/MovieCard.jsx";
+import MovieCard from "../MovieCard/MovieCard.jsx";
 
-function MovieSection() {
+function MovieSection(props) {
+  const key = "89b09e58";
+  const page = 1;
+  const url = `https://www.omdbapi.com/?apikey=${key}&s=${props.searchValue}&page=${page}`;
+
+  const [movieData, setMovieData] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      if (!props.searchValue) return;
+
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        console.log(data.Search);
+
+        setMovieData(data.Search || []);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+    fetchMovies();
+  }, [props.searchValue]);
+
   return (
     <>
       <section className="movie-section">
-        <div className="movie-list container">
+        <div className="movie-container container">
           <h3>Movies</h3>
+
+          <div className="movie-list">
+            {movieData.map((movie) => {
+              return (
+                <MovieCard
+                  Poster={movie.Poster}
+                  Title={movie.Title}
+                  Year={movie.Year}
+                />
+              );
+            })}
+          </div>
         </div>
       </section>
     </>
