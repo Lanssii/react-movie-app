4ba5f32e41861153df5027319be824e8703fad80
lana shotashvili <shotashvililana@gmail.com>
2026-05-25 20:03:20 +0400

    feat: enhance favorites section with no favorites message and movie card rendering

--- src/Components/FavoritesSection/FavoritesSection.jsx
+++ src/Components/FavoritesSection/FavoritesSection.jsx
@@ -1,4 +1,5 @@
 import "./FavoritesSection.css";
+import MovieCard from "../MovieCard/MovieCard.jsx";
 
 function FavoritesSection(props) {
   return (
@@ -6,6 +7,23 @@ function FavoritesSection(props) {
       <section className="favorites-section">
         <div className="favorites-container container">
           <h2>{props.title}</h2>
+          {props.favorites.length === 0 ? (
+            <p className="favorites-info">No favorites added</p>
+          ) : (
+            <div className="favorites-list">
+              {props.favorites.map((movie) => (
+                <MovieCard
+                  key={movie.imdbID}
+                  imdbID={movie.imdbID}
+                  Poster={movie.Poster}
+                  Title={movie.Title}
+                  Year={movie.Year}
+                  favorites={props.favorites}
+                  setFavorites={props.setFavorites}
+                />
+              ))}
+            </div>
+          )}
         </div>
       </section>
     </>
